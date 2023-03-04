0b3b00b84e31e938cb8075f4cd43286655ff4950
MarinaChorna <mrnchrn06@gmail.com>
2023-03-04 16:57:01 +0200

    Завдання 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+const bodyEl = document.querySelector('body');
+
+btnStart.addEventListener("click", () => {
+  colorChange = setInterval(() => {
+    const color = getRandomHexColor();
+    bodyEl.style.backgroundColor = color;
+  }, 1000);
+  btnStart.setAttribute('disabled', true);
+  btnStop.removeAttribute('disabled');
+});
+
+
+btnStop.addEventListener("click", () => {
+  clearInterval(colorChange);
+  btnStart.removeAttribute('disabled');
+  btnStop.setAttribute('disabled', true);
+});
